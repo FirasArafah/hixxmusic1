3c23215f4b63f3cef31d57ce81b9aae3432dabe3
FirasArafah <43936321+FirasArafah@users.noreply.github.com>
2019-03-29 19:10:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,8 +3,8 @@ const client = new Discord.Client();
  
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
-     client.user.setGame("Hix - 1play")
-     client.user.setStatus("idle")
+     client.user.setGame("Hix - 1play");
+     client.user.setStatus("idle");
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
