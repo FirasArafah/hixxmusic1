76d95302e4e795378fb11bc66d3ded63c47bdb6e
FirasArafah <43936321+FirasArafah@users.noreply.github.com>
2019-04-25 22:27:56 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
  
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
-     client.user.setGame("Pullie music.");
+     client.user.setGame("1play");
      client.user.setStatus("idle");
   console.log('')
   console.log('')
